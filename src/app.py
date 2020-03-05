460e148cfb6d6946f36a448f7686dad0823f0da5
MaxiNMedina <maaxii.cb@hotmail.com>
2020-03-05 12:00:51 -0300

    funciona, falta pasar a .png

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ DOMAIN = 'http://sedici.unlp.edu.ar' #Dominio
 URL = 'http://sedici.unlp.edu.ar/handle/10915/74499/discover?search-result=true&query=&current-scope=10915/74499&sort_by=score&order=desc&rpp=300' 
 FILETYPE = '.fits' #tipo de archivos que voy a buscar
 SUB_LINKS_ARRAY = [] #arreglo de sublinks (guarda solo los que dicen 'handle')
-
+file_name = ''
 
 def get_soup(url): #devuele el HTML parseado de la url pasada como parametro
     return bs(requests.get(url).text, 'html.parser')
@@ -24,10 +24,13 @@ for i in range(length):  #para cada sub link vuelvo a hacer lo mismo
     for sub_link2 in soup.find_all('a'): #busco todo lo que tenga 'a'
         file_link = sub_link2.get('href') #me guardo el link del archivo .fits
         if FILETYPE in file_link: # si el tipo de archivo(.fits) esta en el link
-            file_name = soup.title.string + ".fits"  #le asigno como nombre al archivo el titulo + .fits
-            print('Descargando el archivo: ' + file_name + '\n' +'del link: ' + DOMAIN+file_link + '\n') #imprimo que lo voy a descargar
-            with open(file_name, 'wb') as file:
-                response = requests.get(DOMAIN + file_link) #descargo el archivo
-                file.write(response.content) 
+            if ((soup.title.string + ".fits") == file_name): #para que no descargue duplicado
+                continue
+            else:
+                file_name = soup.title.string + ".fits"  #le asigno como nombre al archivo el titulo + .fits
+                print('Descargando el archivo: ' + file_name + '\n' +'del link: ' + DOMAIN+file_link + '\n') #imprimo que lo voy a descargar
+                with open(file_name, 'wb') as file:
+                    response = requests.get(DOMAIN + file_link) #descargo el archivo
+                    file.write(response.content) 
 
-#LO HACE DOS VECES POR CADA LINK PORQUE HAY DOS BOTONES PARA DESCARGAR (TOCANDO IMAGEN O TOCANDO DESCARGAR)
+#FUNCIONA
