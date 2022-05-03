f0a1f254398ebb5f87b252e3c6114c872c75a0a8
Santiago Ponte <saponteahon@gmail.com>
2022-05-03 10:49:37 -0300

    modificaciones menores respecto a la forma de definir la ruta de la api autosave

--- backend/config/routes/spectrum.py
+++ backend/config/routes/spectrum.py
@@ -3,7 +3,6 @@ from app.api import predict
 from app.api import generate_fits
 from app.api import get_metadata
 from app.api import autosave
-from app.api.autosave import autosave_api
    
 def set_routes(app):
     app.add_url_rule("/api/predict", "api_predict",
@@ -12,6 +11,5 @@ def set_routes(app):
                      generate_fits.api_generate_fits, methods=["POST"])
     app.add_url_rule("/api/getMetadata", "api_get_metadata",
                      get_metadata.api_get_metadata, methods=["POST"])
-    
-    #registro usando Blueprints
-    app.register_blueprint(autosave_api) #path = /api/autosave
+    app.add_url_rule("/api/autosave", "save", 
+                     autosave.save, methods=["PUT"])
