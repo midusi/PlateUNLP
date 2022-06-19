6b53f3adfcb0814f7a46e3166556645f3e5ea327
Santiago Ponte <saponteahon@gmail.com>
2022-06-19 17:18:45 -0300

    Modularizacion en componentes .svelte de la zona de filtros

--- frontend/src/components/FilterZone.svelte
+++ frontend/src/components/FilterZone.svelte
@@ -1,5 +1,9 @@
 <script>
   import NButton from "./NButton.svelte";
+  import Brightness from "./Filters/Brightness.svelte";
+  import Contrast from "./Filters/Contrast.svelte";
+  import Colorize from "./Filters/Colorize.svelte";
+  import Zoom from "./Filters/Zoom.svelte";  
 
   import { createEventDispatcher } from 'svelte';
   const dispatch = createEventDispatcher();
@@ -12,94 +16,26 @@
   let color_input = "#ffffff";
 
   function resetSpectrogramCanvas() {
-    dispatch('reset')
-    brightness_input = 0;
-    brightness_input = brightness_input
-    console.log("Brillo = ", brightness_input);
-    contrast_input = 0;
-    color_input = "#ffffff";
-    spectrogramCanvas.resetFilters();
-    console.log("Reset Filters");
-  }
-
-  function setBrightness() {
-    console.log("Brillo = ", brightness_input);
-    spectrogramCanvas.setBrightness(brightness_input);
-  }
-  
-  function setContrast() {
-    console.log("Contraste = ", contrast_input);
-    spectrogramCanvas.setContrast(contrast_input);
-  }
-
-  function colorize() {
-    console.log("Color = ", color_input);
-    spectrogramCanvas.colorize(color_input);
-  }
-
-  function updatescale() {
-    console.log("Scale = ", scale);
-    spectrogramCanvas.setScale(scale);
+      dispatch('reset')
+      brightness_input = 0;
+      contrast_input = 0;
+      color_input = "#ffffff";
+      spectrogramCanvas.resetFilters();
+      console.log("Reset Filters");
   }
 </script>
 
 <div class="card">
   <h5 class="card-header">Filtros</h5>
   <div class="card-body">
-
-    <div class="d-xl-flex flex-column justify-content-xl-start">
-      <span>Zoom</span>
-      <input
-        type="range"
-        min="0.1"
-        max="1"
-        step="0.01"
-        bind:value={scale}
-        on:change={updatescale}
-      />
-    </div>
-
-    <div class="d-xl-flex flex-column justify-content-xl-start">
-      <span>Brillo</span>
-      <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.01"
-        bind:value={brightness_input}
-        on:change={setBrightness} 
-      />
-      <input type="text" value="{brightness_input}" readonly/>
-    </div>
-
-    <div class="d-xl-flex flex-column justify-content-xl-start">
-      <span>Contraste</span>
-      <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.01"
-        bind:value={contrast_input}
-        on:change={setContrast} 
-      />
-      <input type="text" value="{contrast_input}" readonly/>
-    </div>
-
-    <div class="d-xl-flex flex-column justify-content-xl-start">
-      <span>Color</span>
-      <input 
-        type="color" 
-        bind:value={color_input}
-        on:change={colorize} 
-      />
-    </div>
-
+    <Zoom spectrogramCanvas={spectrogramCanvas} bind:scake={scale}/>
+    <Brightness spectrogramCanvas={spectrogramCanvas} bind:brightness_input={brightness_input}/>
+    <Contrast spectrogramCanvas={spectrogramCanvas} bind:contrast_input={contrast_input}/>
+    <Colorize spectrogramCanvas={spectrogramCanvas} bind:color_input={contrast_input}/>
     <p>&nbsp;</p>   
-
     <NButton click={resetSpectrogramCanvas}>
       Reiniciar Filtros
     </NButton>
-
   </div>
 </div>
 <p>&nbsp;</p>    
---
+++ frontend/src/components/Filters/Colorize.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let spectrogramCanvas;
+    export let color_input = 0;
+  
+    function colorize() {
+        console.log("Color = ", color_input);
+        spectrogramCanvas.colorize(color_input);
+    }
+</script>
+  
+<div class="d-xl-flex flex-column justify-content-xl-start">
+    <span>Color</span>
+    <input 
+        type="color" 
+        bind:value={color_input}
+        on:change={colorize} 
+    />
+</div>
