b5b01a6ffc80b672eae1f575341e46c86d2fb99f
Santiago Ponte <santiagoandresponteahon@hotmail.com>
2022-07-10 22:41:07 -0300

    modificando selector de color a cajas clikeables

--- frontend/src/components/Filters/Colorize.svelte
+++ frontend/src/components/Filters/Colorize.svelte
@@ -1,19 +1,56 @@
 <script>
     export let canvas;
-    export let colorize_input = 0;
+    export let colorize_input = "#ffffff";
+
+    let original="#ffffff"
+    let cian="#00ffff"
+    let yellow="#ffff00"
+    let green="#37ff00"
+    
   
     function colorize() {
-        // console.log("Color = ", colorize_input);
+        console.log("Color = ", colorize_input);
         canvas.setColorize(colorize_input);
         canvas.ReRender()
+    }
+
+    function set_original() {
+        colorize_input = original;
+        colorize()
+    }
+
+    function set_cian() {
+        colorize_input = cian;
+        colorize()
+    }
+
+    function set_yellow() {
+        colorize_input = yellow;
+        colorize()
+    }
+
+    function set_green() {
+        colorize_input = green;
+        colorize()
     }
 </script>
   
 <div class="d-xl-flex flex-column justify-content-xl-start">
     <span>Color</span>
-    <input 
-        type="color" 
-        bind:value={colorize_input}
-        on:change={colorize} 
-    />
+    <div>
+        <button style="background:{original};" on:click={set_original}/>
+        <button style="background:{cian};" on:click={set_cian}/>
+        <button style="background:{yellow};" on:click={set_yellow}/>
+        <button style="background:{green};" on:click={set_green}/>
+    </div>
 </div>
+
+<style>
+    button {
+        font-size: 20px;
+        line-height: 50px;
+        margin:2px;
+        width: 10px;
+        height: 10px;
+    }
+</style>
