92727c7dff26135e8c2ac1735778e4af52fcc426
Santiago Ponte <santiagoandresponteahon@hotmail.com>
2022-10-08 19:35:51 -0300

    Refactoring en el componente PlateForm.svelte del Frontend

--- frontend/src/components/PlateForm.svelte
+++ frontend/src/components/PlateForm.svelte
@@ -15,33 +15,19 @@
     plateData["SOFTWARE"] = getSoftwareDef();
 </script>
 
-    <div class="px-4" style=" border: 1px solid  ;">
-        {#each arr as field}
-            <div class="form-row">
+<div class="px-4" style=" border: 1px solid  ;">
+    {#each arr as fields}
+        <div class="form-row">
+            {#each fields as field}
                 <div class="form-group col-md-4">
-                    {#if field[0] !== undefined}
+                    {#if field !== undefined}
                         <Field
-                            name={field[0]}
-                            bind:value={plateData[field[0]]}
+                            name={field}
+                            bind:value={plateData[field]}
                         />
                     {/if}
                 </div>
-                <div class="form-group col-md-4">
-                    {#if field[1] !== undefined}
-                        <Field
-                            name={field[1]}
-                            bind:value={plateData[field[1]]}
-                        />
-                    {/if}
-                </div>
-                <div class="form-group col-md-4">
-                    {#if field[2] !== undefined}
-                        <Field
-                            name={field[2]}
-                            bind:value={plateData[field[2]]}
-                        />
-                    {/if}
-                </div>
-            </div>
-        {/each}
-    </div>
+            {/each}
+        </div>
+    {/each}
+</div>
