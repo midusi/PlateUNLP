db2bb66210cd49e657ab31419b08b45ac84eacc9
JuanM04 <me@juanm04.com>
2024-09-30 18:15:34 -0300

    💄 Added icon and title

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,10 @@ import MaterialReferenceForm from "./components/LampReferenceForm"
 export default function App() {
   return (
     <main className="max-w-4xl mx-auto">
+      <h1 className="text-center mt-12 mb-16 text-4xl font-bold tracking-tight lg:text-5xl">
+        🌌 PlateUNLP
+      </h1>
+
       <MaterialReferenceForm />
       <FitsLoader />
     </main>
