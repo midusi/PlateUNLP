9ad9ebf5025d736ef9dde77fbe57bad961534bb2
JuanM04 <me@juanm04.com>
2024-09-30 16:22:54 -0300

    ➕ Added shadcn/ui

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
@@ -6,5 +7,6 @@ import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>,
 )
