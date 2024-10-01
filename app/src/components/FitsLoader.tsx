5fb6639d1377ba78416971f1a64020e9e0d277f8
JuanM04 <me@juanm04.com>
2024-10-01 01:11:04 -0300

    🥅 Maybe now will work on prod

--- app/src/components/FitsLoader.tsx
+++ app/src/components/FitsLoader.tsx
@@ -1,44 +1,53 @@
-import { useState } from "react"
-import FitsViewer from "./FitsViewer"
-import "fitsjs"
+import { useState } from "react";
+import FitsViewer from "./FitsViewer";
+import "fitsjs";
 
 export default function FitsLoader({}) {
-  const [loading, setLoading] = useState(false)
-  const [fileContent, setFileContent] = useState<any | null>(null)
+  const [loading, setLoading] = useState(false);
+  const [fileContent, setFileContent] = useState<any | null>(null);
 
-  console.log(fileContent)
+  console.log(fileContent);
   function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     if (event.target.files) {
-      const file = event.target.files[0]
-      setLoading(true)
-
-      // Callback para manejar el archivo FITS una vez cargado
-      const callback = function () {
-        const hdu = this.getHDU() // Obtiene la primera unidad de encabezado-datos
-        const header = hdu.header // Obtiene el encabezado
-        const dataunit = hdu.data // Obtiene la unidad de datos
-
-        // Procesar datos como desees
-        console.log("Header:", header)
-        console.log("Data:", dataunit)
-
-        // Almacenar los resultados en el estado
-        setFileContent({ header, dataunit })
-        setLoading(false)
-      }
+      const file = event.target.files[0];
+      setLoading(true);
 
       // Inicializa el objeto FITS utilizando el archivo
-      const fits = new astro.FITS.File(file, callback)
+      try {
+        // Callback para manejar el archivo FITS una vez cargado
+        const callback = function () {
+          const hdu = this.getHDU(); // Obtiene la primera unidad de encabezado-datos
+          const header = hdu.header; // Obtiene el encabezado
+          const dataunit = hdu.data; // Obtiene la unidad de datos
+
+          // Procesar datos como desees
+          console.log("Header:", header);
+          console.log("Data:", dataunit);
+
+          // Almacenar los resultados en el estado
+          setFileContent({ header, dataunit });
+          setLoading(false);
+        };
+
+        const fits = new astro.FITS.File(file, callback);
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
 
   return (
     <>
-      <input type="file" id="fileInput" onChange={handleFileChange} accept=".fits" />
+      <input
+        type="file"
+        id="fileInput"
+        onChange={handleFileChange}
+        accept=".fits"
+      />
       {loading && <p>Cargando contenido...</p>}
       {fileContent && <FitsViewer content={fileContent} />}
     </>
-  )
+  );
 }
 
 // function parseFits(buffer: ArrayBuffer): FitsHeader {
