a87cfd99ee19ab8515b43c342a5dc3966e2a1e2e
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-10-18 11:21:17 -0300

    :sparkles:  Funcion para convertir fits 1D -> json

--- utils_py/jsonifyFits1D.py
+++ utils_py/jsonifyFits1D.py
@@ -1,5 +1,7 @@
-import json
 import os
+import json
+import numpy as np
+from astropy.io import fits
 
 class FileFinder:
     folder_path:str
@@ -22,15 +24,61 @@ class FileFinder:
                         break
         return filepaths
 
-def jsonifyFits1D():
-    folder_path = "C:\\Users\\santi\\OneDrive\\Documentos\\Doctorado\\Datos Crudos"
-    ff = FileFinder(folder_path=folder_path)
-    start_array1 = ["WOBJ", "WCOMP"]
-    start_array2 = ["EFBTOBJ", "EFBTCOMP"]
-    filesW = ff.filename_in_folder(startwhith=start_array1)
-    filesW = filesW["WOBJ"] + filesW["WCOMP"]
-    filesEFBT = ff.filename_in_folder(startwhith=start_array2)
-    filesEFBT = filesEFBT["EFBTOBJ"] + filesEFBT["EFBTCOMP"]
+def extract_lamp_info(filepath:str, normalize:bool=False):
+    """Funcion para obtener los datos de un archivo correspondiente a una lampara de comparación
+
+    Args:
+        filepath (str, optional): Direccion del archivo.
+        normalize (bool, optional): Booleano para saber si los datos de respuesta deben estar. 
+        normalizados o no. Defaults to False.
+
+    Returns:
+        numpy.ndarray: Datos de la lampara correspondientes al eje X
+        numpy.ndarray: Datos de la lampara correspondientes al eje Y
+        list: Headers adjuntos al archivo
+    """
+    # Extraer datos y headers del archivo
+    hdul = fits.open(filepath) 
+    if('WOBJ' in filepath): # Espectro calibrado
+        headers = hdul[0].header
+        data = hdul[0].data[0][0]
+    else:
+        headers = hdul[0].header
+        data = hdul[0].data
     
-    function_json = {}
-    return function_json
+    # Separ datos en X e Y
+    obs_x = np.array(range(len(data)))
+    obs_y = data
+    
+    # Normalizado de los datos obserbados en el eje Y
+    if (normalize):
+        obs_y, _, _ = normalize_min_max(obs_y)
+    
+    return obs_x, obs_y, headers
+
+def jsonifyFits1D(path:str):
+    #folder_path = "C:\\Users\\santi\\OneDrive\\Documentos\\Doctorado\\Datos Crudos" #Home
+    folder_path = "/home/sponte/Documentos/Doctorado/AppAlineado/AppAlineado/db/exampleLampsFiles/Conjunto_Original(HeNeAr)/WCOMPs" #Lab
+    ff = FileFinder(folder_path=folder_path)
+    start_array = ["WOBJ", "WCOMP"]
+    filesW = ff.filename_in_folder(startwhith=start_array)
+    filesW = filesW["WOBJ"] + filesW["WCOMP"]
+
+    folder_path = "/home/sponte/Documentos/Doctorado/AppAlineado/AppAlineado/db/exampleLampsFiles/Conjunto_Original(HeNeAr)/EFBTCOMPs" #Lab
+    ff = FileFinder(folder_path=folder_path)
+    start_array = ["EFBTOBJ", "EFBTCOMP"]
+    filesEFBT = ff.filename_in_folder(startwhith=start_array)
+    filesEFBT = filesEFBT["EFBTOBJ"] + filesEFBT["EFBTCOMP"]
+
+    for file in filesW:
+        name = file.split('/')[-1].split('.')[0]
+        print(name)
+        emp_x, emp_y, emp_head = extract_lamp_info(file, normalize=False)
+
+        for_json = [{"pix":str(it[0]), "wav":str(it[1])} for it in zip(emp_x, emp_y)]
+
+        with open(os.path.join(path,f"{name}.json"), "w") as archivo_json:
+            json.dump(for_json, archivo_json, indent=4)
+
+path_save = '/home/sponte/Documentos/Doctorado/AppAlineado/AppAlineado/db/exampleLampsFiles/Conjunto_Original(HeNeAr)/WCOMPs_JSON'
+jsonifyFits1D(path=path_save)
