cf2f9aab71aa72e83289e5cbe03d5f06baaeffb1
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-12-08 18:11:24 -0300

    :alembic: Agregando segundo eje X

--- app/src/components/EmpiricalSpectrum.tsx
+++ app/src/components/EmpiricalSpectrum.tsx
@@ -1,7 +1,7 @@
 import type { JSX } from "react/jsx-runtime"
 import { useGlobalStore } from "@/hooks/use-global-store"
 import { useMeasure } from "@/hooks/use-measure"
-import { AxisBottom, AxisLeft } from "@visx/axis"
+import { AxisBottom, AxisLeft, AxisTop } from "@visx/axis"
 import { curveLinear } from "@visx/curve"
 import { GridColumns, GridRows } from "@visx/grid"
 import { Group } from "@visx/group"
@@ -20,13 +20,13 @@ const getX = (p: EmpiricalSpectrumPoint) => p?.pixel ?? 0
 const getY = (p: EmpiricalSpectrumPoint) => p?.intensity ?? 0
 
 const height = 300
-const margin = { top: 40, right: 30, bottom: 50, left: 55 }
+const margin = { top: 40, right: 30, bottom: 100, left: 55 }
 
 export function EmpiricalSpectrum({ data, color }: { data: EmpiricalSpectrumPoint[], color: string }) {
-  const { xScale, yScale } = useMemo(() => {
+  const { xScalePixel, yScale } = useMemo(() => {
     return {
       data,
-      xScale: scaleLinear<number>({ domain: [0, d3.max(data, getX)!] }),
+      xScalePixel: scaleLinear<number>({ domain: [0, d3.max(data, getX)!] }),
       yScale: scaleLinear<number>({ domain: [0, d3.max(data, getY)!] }),
     }
   }, [data])
@@ -38,7 +38,7 @@ export function EmpiricalSpectrum({ data, color }: { data: EmpiricalSpectrumPoin
   const yMax = Math.max(height - margin.top - margin.bottom, 0)
 
   // update scale output ranges
-  xScale.range([0, xMax])
+  xScalePixel.range([0, xMax])
   yScale.range([yMax, 0])
 
   // Point logic
@@ -51,7 +51,7 @@ export function EmpiricalSpectrum({ data, color }: { data: EmpiricalSpectrumPoin
 
   const spotsInGraph: JSX.Element[] = []
   for (const [index, point] of lampPoints.entries()) {
-    const xClick = xScale(point.x)
+    const xClick = xScalePixel(point.x)
     // const yPix = (height - margin.bottom) - (height - margin.bottom - margin.top - yScale(point.y))
     spotsInGraph.push(
       <g key={`EmpiricalLine-${index}`}>
@@ -81,7 +81,7 @@ export function EmpiricalSpectrum({ data, color }: { data: EmpiricalSpectrumPoin
   function onClick(event: React.MouseEvent<SVGSVGElement>) {
     const svgRect = event.currentTarget.getBoundingClientRect()
     const xClick = event.clientX - svgRect.left - margin.left
-    const xVal = xScale.invert(xClick)
+    const xVal = xScalePixel.invert(xClick)
     const yMatch = data[Math.round(xVal) - 1]
     if (yMatch) {
       const newVal = {
@@ -111,7 +111,7 @@ export function EmpiricalSpectrum({ data, color }: { data: EmpiricalSpectrumPoin
         {spotsInGraph}
         <Group top={margin.top} left={margin.left}>
           <GridColumns
-            scale={xScale}
+            scale={xScalePixel}
             width={xMax}
             height={yMax}
             className="stroke-neutral-100"
@@ -125,16 +125,21 @@ export function EmpiricalSpectrum({ data, color }: { data: EmpiricalSpectrumPoin
           <LinePath<EmpiricalSpectrumPoint>
             curve={curveLinear}
             data={data}
-            x={p => xScale(getX(p)) ?? 0}
+            x={p => xScalePixel(getX(p)) ?? 0}
             y={p => yScale(getY(p)) ?? 0}
             shapeRendering="geometricPrecision"
             className="stroke-1"
             style={{ stroke: color }}
           />
-          <AxisBottom
-            scale={xScale}
-            top={yMax}
+          <AxisTop
+            scale={xScalePixel}
             label="Pixel"
+            numTicks={Math.floor(xMax / 80)}
+          />
+          <AxisBottom
+            scale={xScalePixel}
+            top={yMax}
+            label="Wavelength (Å)"
             numTicks={Math.floor(xMax / 80)}
           />
           <AxisLeft scale={yScale} label="Intensity" />
