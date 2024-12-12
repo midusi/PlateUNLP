693dfb7a129ac87b92a09e769af87b26a633a448
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-12-12 16:44:17 -0300

    :lipstick: Nueva distribucion de componentes en la UI

--- app/src/components/StepCalibration.tsx
+++ app/src/components/StepCalibration.tsx
@@ -12,34 +12,34 @@ export function StepCalibration() {
 
     return (
         <>
-            <ReferenceLampForm />
-
-            <section className="space-y-0 my-8">
-                <ReferenceLampRange />
-                <ReferenceLampSpectrum />
-                <div>
-                    <h1 className="text-2xl font-bold">Empirical Comparison Lamp</h1>
-                    <FitsLoader
-                        plotColor="#0ea5e9"
-                        setData={() => { }}
-                        interactable
-                        preview
-                    />
-                </div>
-                <div>
-                    <h1 className="text-2xl font-bold">Empirical Spectrum</h1>
-                    <FitsLoader
-                        plotColor="#16a34a"
-                        setData={setScienceSpectrumData}
-                        interactable={false}
-                        preview
-                    />
-                </div>
-                <div>
+            <div className="flex w-full">
+                <div className="w-1/4 bg-gray-200 p-4">
+                    <ReferenceLampForm />
                     <h1 className="text-2xl font-bold">Inference function fit</h1>
                     <InferenceForm />
                 </div>
-            </section>
+
+                <div className="w-3/4 bg-white-100 p-4">
+                    <section className="space-y-0 my-8">
+                        <ReferenceLampRange />
+                        <ReferenceLampSpectrum />
+                        <h1 className="text-2xl font-bold">Empirical Comparison Lamp</h1>
+                        <FitsLoader
+                            plotColor="#0ea5e9"
+                            setData={() => { }}
+                            interactable
+                            preview
+                        />
+                        <h1 className="text-2xl font-bold">Empirical Spectrum</h1>
+                        <FitsLoader
+                            plotColor="#16a34a"
+                            setData={setScienceSpectrumData}
+                            interactable={false}
+                            preview
+                        />
+                    </section>
+                </div>
+            </div>
 
             <ContinueButton data={scienceSpectrumData} />
         </>
