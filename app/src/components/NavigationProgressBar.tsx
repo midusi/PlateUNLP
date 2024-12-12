93081ace1bcf5ecc13535e8795274ae42233d36e
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-12-12 20:27:30 -0300

    :lipstick: Remplazando textos de Next y Prev por flechas a izquierda y a derecha respectivamente

--- app/src/components/NavigationProgressBar.tsx
+++ app/src/components/NavigationProgressBar.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline"
 import { useState } from "react"
 import { Button } from "./ui/button"
 import "./css/ProgressBar.css"
@@ -40,8 +41,12 @@ export function NavigationProgressBar({ stepsArr, initialStep }: NavigationProgr
     return (
         <div className="w-full space-y-4">
             <div className="flex justify-between mt-4">
-                <Button onClick={() => simulateProgress(-1)} disabled={progress === min}>Prev</Button>
-                <Button onClick={() => simulateProgress(1)} disabled={progress === max}>Next</Button>
+                <Button onClick={() => simulateProgress(-1)} disabled={progress === min}>
+                    <ChevronLeftIcon className="h-5 w-5" />
+                </Button>
+                <Button onClick={() => simulateProgress(1)} disabled={progress === max}>
+                    <ChevronRightIcon className="h-5 w-5" />
+                </Button>
             </div>
             <ProgressBar value={progress} max={max} />
             <div className="relative w-full">
@@ -72,8 +77,12 @@ export function NavigationProgressBar({ stepsArr, initialStep }: NavigationProgr
             </div>
 
             <div className="flex justify-between mt-4">
-                <Button onClick={() => simulateProgress(-1)} disabled={progress === min}>Prev</Button>
-                <Button onClick={() => simulateProgress(1)} disabled={progress === max}>Next</Button>
+                <Button onClick={() => simulateProgress(-1)} disabled={progress === min}>
+                    <ChevronLeftIcon className="h-5 w-5" />
+                </Button>
+                <Button onClick={() => simulateProgress(1)} disabled={progress === max}>
+                    <ChevronRightIcon className="h-5 w-5" />
+                </Button>
             </div>
         </div>
     )
