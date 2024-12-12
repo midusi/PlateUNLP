72fa36e8c712ac135a260e17523f1f8125959db7
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-12-12 17:48:39 -0300

    :lipstick: Region de funciones ajustes de posiciones y fijado de espacios de cada funcion

--- app/src/components/StepCalibration.tsx
+++ app/src/components/StepCalibration.tsx
@@ -6,6 +6,7 @@ import { ReferenceLampForm } from "@/components/ReferenceLampForm"
 import { ReferenceLampRange } from "@/components/ReferenceLampRange"
 import { ReferenceLampSpectrum } from "@/components/ReferenceLampSpectrum"
 import { useState } from "react"
+import { CardTitle } from "./ui/card"
 
 export function StepCalibration() {
     const [scienceSpectrumData, setScienceSpectrumData] = useState<EmpiricalSpectrumPoint[] | null>(null)
@@ -19,22 +20,33 @@ export function StepCalibration() {
                 </div>
 
                 <div className="w-3/4 bg-gray-100 p-4">
+                    <CardTitle>Teorical Comparison Lamp</CardTitle>
                     <ReferenceLampRange />
-                    <ReferenceLampSpectrum />
-                    <h1 className="text-2xl font-bold">Empirical Comparison Lamp</h1>
-                    <FitsLoader
-                        plotColor="#0ea5e9"
-                        setData={() => { }}
-                        interactable
-                        preview
-                    />
-                    <h1 className="text-2xl font-bold">Empirical Spectrum</h1>
-                    <FitsLoader
-                        plotColor="#16a34a"
-                        setData={setScienceSpectrumData}
-                        interactable={false}
-                        preview
-                    />
+                    <div className="flex flex-col h-screen">
+                        <div className="flex-1">
+                            <ReferenceLampSpectrum />
+                        </div>
+                        <div className="flex-1">
+                            <CardTitle>Empirical Comparison Lamp</CardTitle>
+                            <FitsLoader
+                                plotColor="#0ea5e9"
+                                setData={() => { }}
+                                interactable
+                                preview
+                            />
+                        </div>
+
+                        <div className="flex-1">
+                            <CardTitle>Empirical Spectrum</CardTitle>
+                            <FitsLoader
+                                plotColor="#16a34a"
+                                setData={setScienceSpectrumData}
+                                interactable={false}
+                                preview
+                            />
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
