fa13f0a4a93312f7e3b9f8be066ab43cb0ce14cb
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-12-20 21:29:23 -0300

    :bug: Espacios definidos para ReferenceLampRange y Botones Min y Max, :sparkles: los espacios se redimensionan de forma dinamica

--- app/src/components/ReferenceLampRangeUI.tsx
+++ app/src/components/ReferenceLampRangeUI.tsx
@@ -1,7 +1,14 @@
-import { ReferenceLampForm } from "./ReferenceLampForm"
+import { ReferenceLampRange } from "./ReferenceLampRange"
 
 export function ReferenceLampRangeUI() {
     return (
-        <ReferenceLampForm />
+        <div className="flex w-full overflow-hidden">
+            <div className="flex-8" style={{ width: `80%` }}>
+                <ReferenceLampRange />
+            </div>
+            <div className=" bg-green-100 p-4 flex-2 w-[200px]">
+                Botones min y max
+            </div>
+        </div>
     )
 }
