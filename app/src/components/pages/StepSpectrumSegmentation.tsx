d53cd3573c2aec9bf0c21f0eed1afcd36feb1115
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-02-06 17:31:54 -0300

    :sparkles: Obtención y graficado de BBs

--- app/src/components/pages/StepSpectrumSegmentation.tsx
+++ app/src/components/pages/StepSpectrumSegmentation.tsx
@@ -4,8 +4,9 @@ import type { ChangeEvent } from "react"
 import { Button } from "@/components/atoms/button"
 import { Uploader } from "@/components/molecules/Uploader"
 import { BBImageEditor } from "@/components/organisms/BBImageEditor"
+import { Spectrum } from "@/enums/Spectrum"
 import * as ort from "onnxruntime-web"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 type LoadingState = "waiting" | "processing" | "finished" | "error"
 
@@ -46,10 +47,10 @@ interface SegmentationUIProps {
 function SegmentationUI({ file, onComplete }: SegmentationUIProps) {
     const [boundingBoxes, setBoundingBoxes] = useState<BoundingBox[]>([])
 
-    useEffect(() => {
-        // Predecir Bounding Boxes
-        determineBB(file)
-    }, [])
+    async function handleAutodetect() {
+        const bbAutodetected = await determineBBs(file)
+        setBoundingBoxes(bbAutodetected)
+    }
 
     async function handleDownload() {
         if (!file || boundingBoxes.length === 0)
@@ -104,6 +105,13 @@ function SegmentationUI({ file, onComplete }: SegmentationUIProps) {
 
     return (
         <>
+            <Button
+                onClick={() => {
+                    handleAutodetect()
+                }}
+            >
+                Autodetect
+            </Button>
             <BBImageEditor className="w-full" src={file} boundingBoxes={boundingBoxes} setBoundingBoxes={setBoundingBoxes} />
             <div className="flex justify-center pt-4">
                 <Button
@@ -120,91 +128,135 @@ function SegmentationUI({ file, onComplete }: SegmentationUIProps) {
     )
 }
 
-function prepare_input(img_src: string) {
-    const image = new Image()
-    image.src = img_src
+async function determineBBs(img_src: string) {
+    const SIZE_M = 1088
+    const CLASSES = [Spectrum.Lamp, Spectrum.Science]
+    const THRESHOLD = 0.8
 
-    const canvas = document.createElement("canvas")
-    canvas.width = 1088
-    canvas.height = 1088
-    const context = canvas.getContext("2d")!
-    context.drawImage(image, 0, 0, 1088, 1088)
-
-    const data = context.getImageData(0, 0, 1088, 1088).data
-    const red: number[] = []
-    const green: number[] = []
-    const blue: number[] = []
-    for (let index = 0; index < data.length; index += 4) {
-        red.push(data[index] / 255)
-        green.push(data[index + 1] / 255)
-        blue.push(data[index + 2] / 255)
+    async function loadModel() {
+        const model_path = "/models/spectrum_part_segmentator.onnx"
+        const session = await ort.InferenceSession.create(model_path)
+        return session
     }
-    return {
-        input: new Float32Array([...red, ...green, ...blue]),
-        image,
+
+    function prepare_input(img_src: string) {
+        const image = new Image()
+        image.src = img_src
+
+        const canvas = document.createElement("canvas")
+        canvas.width = SIZE_M
+        canvas.height = SIZE_M
+        const context = canvas.getContext("2d")!
+        context.drawImage(image, 0, 0, SIZE_M, SIZE_M)
+
+        const data = context.getImageData(0, 0, SIZE_M, SIZE_M).data
+        const red: number[] = []
+        const green: number[] = []
+        const blue: number[] = []
+        for (let index = 0; index < data.length; index += 4) {
+            red.push(data[index] / 255)
+            green.push(data[index + 1] / 255)
+            blue.push(data[index + 2] / 255)
+        }
+        return {
+            input: new Float32Array([...red, ...green, ...blue]),
+            image,
+        }
     }
-}
 
-async function loadModel() {
-    const model_path = "/models/spectrum_part_segmentator.onnx"
-    const session = await ort.InferenceSession.create(model_path)
-    return session
-}
+    async function runInference(session: ort.InferenceSession, input: Float32Array<ArrayBuffer>) {
+        const feeds = { images: new ort.Tensor("float32", input, [1, 3, SIZE_M, SIZE_M]) }
+        const outputs = await session.run(feeds)
+        return outputs
+    }
 
-async function runInference(session: ort.InferenceSession, input: Float32Array<ArrayBuffer>) {
-    const feeds = { images: new ort.Tensor("float32", input, [1, 3, 1088, 1088]) }
-    const outputs = await session.run(feeds)
-    return outputs
-}
+    function processOutputs(outputs: ort.InferenceSession.OnnxValueMapType, image: HTMLImageElement) {
+        const { naturalWidth: NATURALWIDTH, naturalHeight: NATURALHEIGHT } = image
 
-async function determineBB(img_src: string) {
-    const yolo_classes = ["lamp spectrum", "science spectrum"]
+        // console.log("salida", outputs.output0.data)
+        const DATA = outputs.output0.data as Float32Array
+        const COLS = outputs.output0.dims[2]
+        const ROWS = outputs.output0.dims[1]
+
+        let boundingBoxes: BoundingBox[] = []
+        let id = 0
+        for (let column = 0; column < COLS; column++) {
+            const [class_id, prob] = [...Array.from({ length: ROWS - 4 }).keys()]
+                .map(row => [row, DATA[COLS * (row + 4) + column]])
+                .reduce((accum, item) => item[1] > accum[1] ? item : accum, [0, 0])
+
+            if (prob < THRESHOLD) {
+                continue
+            }
+
+            const label = CLASSES[class_id]
+            const xc: number = DATA[COLS * 0 + column]
+            const yc: number = DATA[COLS * 1 + column]
+            const w: number = DATA[COLS * 2 + column] / SIZE_M * NATURALWIDTH
+            const h: number = DATA[COLS * 3 + column] / SIZE_M * NATURALWIDTH
+
+            const x1 = (xc - w / 2) / SIZE_M * NATURALWIDTH
+            const y1 = (yc - h / 2) / SIZE_M * NATURALHEIGHT
+
+            const boundingBox: BoundingBox = {
+                id: id++,
+                name: label,
+                x: x1,
+                y: y1,
+                width: w,
+                height: h,
+                class_name: CLASSES[class_id],
+                prob,
+            }
+
+            boundingBoxes.push(boundingBox)
+        }
+
+        boundingBoxes = boundingBoxes.sort((bb1, bb2) => bb2.prob - bb1.prob)
+        const result = []
+        while (boundingBoxes.length > 0) {
+            result.push(boundingBoxes[0])
+            boundingBoxes = boundingBoxes.filter(bb => iou(boundingBoxes[0], bb) < 0.7)
+        }
+
+        return result
+    }
 
     const session: ort.InferenceSession = await loadModel()
     const { input, image } = prepare_input(img_src)
     const outputs: ort.InferenceSession.OnnxValueMapType = await runInference(session, input)
-    const processed = processOutputs(outputs, image)
-    // console.log("Resultado de la inferencia:", outputs)
+    const processed_output: BoundingBox[] = processOutputs(outputs, image)
+
+    return processed_output
 }
 
-function processOutputs(outputs: ort.InferenceSession.OnnxValueMapType, image: HTMLImageElement) {
-    const { naturalWidth: NATURALWIDTH, naturalHeight: NATURALHEIGHT } = image
-    console.log("Hola: ", NATURALHEIGHT, NATURALWIDTH)
-
-    // console.log("salida", outputs.output0.data)
-    const DATA = outputs.output0.data
-    const COLS = outputs.output0.dims[2]
-    const ROWS = outputs.output0.dims[1]
-    let column, row
-    for (column = 0; column < COLS; column++) {
-        const xc = DATA[8400 * 0 + column]
-        const yc = DATA[8400 * 1 + column]
-        const w = DATA[8400 * 2 + column]
-        const h = DATA[8400 * 3 + column]
-
-        // const x1 = (xc - w / 2) / 640 * img_width
-        // const y1 = (yc - h / 2) / 640 * img_height
-        // const x2 = (xc + w / 2) / 640 * img_width
-        // const y2 = (yc + h / 2) / 640 * img_height
-    }
-    return outputs
-}
-
-function iou(box1, box2) {
+function iou(box1: BoundingBox, box2: BoundingBox) {
     return intersection(box1, box2) / union(box1, box2)
 }
 
-function union(box1, box2) {
-    const [box1_x1, box1_y1, box1_x2, box1_y2] = box1
-    const [box2_x1, box2_y1, box2_x2, box2_y2] = box2
+function union(box1: BoundingBox, box2: BoundingBox) {
+    const { x: box1_x1, y: box1_y1, width: box1_width, height: box1_height } = box1
+    const box1_x2 = box1_x1 + box1_width
+    const box1_y2 = box1_y1 + box1_height
+
+    const { x: box2_x1, y: box2_y1, width: box2_width, height: box2_height } = box2
+    const box2_x2 = box2_x1 + box2_width
+    const box2_y2 = box2_y1 + box2_height
+
     const box1_area = (box1_x2 - box1_x1) * (box1_y2 - box1_y1)
     const box2_area = (box2_x2 - box2_x1) * (box2_y2 - box2_y1)
     return box1_area + box2_area - intersection(box1, box2)
 }
 
-function intersection(box1, box2) {
-    const [box1_x1, box1_y1, box1_x2, box1_y2] = box1
-    const [box2_x1, box2_y1, box2_x2, box2_y2] = box2
+function intersection(box1: BoundingBox, box2: BoundingBox) {
+    const { x: box1_x1, y: box1_y1, width: box1_width, height: box1_height } = box1
+    const box1_x2 = box1_x1 + box1_width
+    const box1_y2 = box1_y1 + box1_height
+
+    const { x: box2_x1, y: box2_y1, width: box2_width, height: box2_height } = box2
+    const box2_x2 = box2_x1 + box2_width
+    const box2_y2 = box2_y1 + box2_height
+
     const x1 = Math.max(box1_x1, box2_x1)
     const y1 = Math.max(box1_y1, box2_y1)
     const x2 = Math.min(box1_x2, box2_x2)
