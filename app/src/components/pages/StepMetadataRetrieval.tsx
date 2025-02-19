27694599c6dcced24b1b647c44c23e2f71172be7
Lautaro Pereyra <heelios12@gmail.com>
2025-02-19 11:50:24 -0300

    Se modificaron los botones del paso StepMetadataRetrieval.

--- app/src/components/pages/StepMetadataRetrieval.tsx
+++ app/src/components/pages/StepMetadataRetrieval.tsx
@@ -5,58 +5,24 @@ import { Button } from "../atoms/button"
 import { SpectrumMetadataForm } from "../molecules/SpectrumMetadataForm"
 
 export function StepMetadataRetrieval({ onComplete }: StepProps) {
-    const spectrumMetadataFormRef = useRef<{ setValues: (spectrumMetadata: SpectrumMetadata) => void, getValues: () => SpectrumMetadata, validate: () => void }>(null)
+    const spectrumMetadataFormRef = useRef<{ setValues: (spectrumMetadata: SpectrumMetadata) => void, resetValues: () => void, getValues: () => SpectrumMetadata, validate: () => void }>(null)
     return (
         <>
             <SpectrumMetadataForm ref={spectrumMetadataFormRef} />
-            <div className="flex mt-4">
+            <div className="flex justify-evenly mt-6">
                 <Button
-                    onClick={() => console.log(spectrumMetadataFormRef.current?.getValues())}
-                    className="bg-blue-500 text-white p-2 rounded"
+                    onClick={() => spectrumMetadataFormRef.current?.resetValues()}
+                    className=" bg-blue-500 w-1/4"
                 >
-                    Get
+                    Reset fields
                 </Button>
-                <Button
-                    onClick={() => console.log(`formulario correcto: ${spectrumMetadataFormRef.current?.validate()}`)}
-                    className="bg-blue-500 text-white p-2 rounded"
+
+                <Button className="w-1/4"
+                    onClick={() => { if (spectrumMetadataFormRef.current?.validate()) onComplete() }}
                 >
-                    Validate
-                </Button>
-                <Button
-                    onClick={() => spectrumMetadataFormRef.current?.setValues({
-                        OBJECT: "defult",
-                        DATE_OBS: new Date(),
-                        TIME_OBS: 0,
-                        MAIN_ID: "default-id",
-                        UT: 0,
-                        ST: 0,
-                        HA: 0,
-                        RA: 0,
-                        DEC: 0,
-                        GAIN: 0,
-                        RA2000: 0,
-                        DEC2000: 0,
-                        RA1950: 0,
-                        DEC1950: 0,
-                        EXPTIME: 0,
-                        DETECTOR: "",
-                        IMAGETYP: "",
-                        SPTYPE: "",
-                        JD: 0,
-                        EQUINOX: 0,
-                    })}
-                    className=" bg-blue-500 text-white p-2 rounded"
-                >
-                    Set
+                    Save
                 </Button>
             </div>
-
-            <Button
-                onClick={() => { onComplete() }}
-                disabled
-            >
-                Save
-            </Button>
         </>
     )
 }
