18087b715e5575595455207c4451883a103cb7d0
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-02-20 19:24:45 -0300

    :construction: Barra de navegacion con caja de fondo en los elementos de la segunda seccion

--- app/src/components/organisms/NewNavigationProgressBar.tsx
+++ app/src/components/organisms/NewNavigationProgressBar.tsx
@@ -19,44 +19,11 @@ export function NewNavigationProgressBar({ general, perSpectrum }: NewNavigation
     ]
 
     return (
-        <NavigationlineWithBox
+        <NavigationLine
             steps={steps}
             actualStep={actual}
             setActualStep={setActual}
         />
-    )
-}
-
-interface NavigationlineWithBoxProps {
-    steps: JSX.Element[]
-    actualStep: number
-    setActualStep: React.Dispatch<React.SetStateAction<number>>
-}
-
-function NavigationlineWithBox({ steps, actualStep, setActualStep }: NavigationlineWithBoxProps) {
-    const groups = [
-        steps.slice(0, 4),
-        steps.slice(4),
-    ]
-    const percents = groups.map(group => Math.round(100 * group.length / steps.length))
-
-    return (
-        <>
-            <div className="flex items-center w-full">
-                <div
-                    className="flex items-center justify-between border rounded-lg px-4 py-2 bg-gray-100"
-                    style={{ flexBasis: `${percents[0]}%` }}
-                >
-                    <NavigationLine steps={groups[0]} actualStep={0} setActualStep={setActualStep} />
-                </div>
-                <div
-                    className="flex items-center justify-between border rounded-lg px-4 py-2 bg-gray-100"
-                    style={{ flexBasis: `${percents[1]}%` }}
-                >
-                    <NavigationLine steps={groups[1]} actualStep={0} setActualStep={setActualStep} />
-                </div>
-            </div>
-        </>
     )
 }
 
@@ -67,28 +34,42 @@ interface NavigationLineProps {
 }
 
 function NavigationLine({ steps, actualStep, setActualStep }: NavigationLineProps) {
+    const components = steps.map((_, index) => (
+        <div key={index} className="flex items-center w-full last:w-auto">
+            <span
+                className={clsx(
+                    "rounded-full",
+                    index < actualStep ? "bg-blue-500" : "bg-gray-500",
+                    index === actualStep ? "w-5 h-5" : "w-4 h-4",
+                    "cursor-pointer active:scale-90 active:bg-blue-700",
+                )}
+                onClick={_ => setActualStep(index)}
+            />
+            {index < steps.length - 1 && (
+                <div className={clsx(
+                    "h-1 flex-1",
+                    index < actualStep ? "bg-blue-500" : "bg-gray-500",
+                )}
+                />
+            )}
+        </div>
+    ))
+
+    const slicePoint: number = 2
     return (
-        <div className="flex items-center justify-between w-full">
-            {steps.map((_: JSX.Element, index: number) => (
-                <div key={index} className="flex items-center w-full last:w-auto">
-                    <span
-                        className={clsx(
-                            "rounded-full",
-                            index < actualStep ? "bg-blue-500" : "bg-gray-500",
-                            index === actualStep ? "w-5 h-5" : "w-4 h-4",
-                            "cursor-pointer active:scale-90 active:bg-blue-700",
-                        )}
-                        onClick={_ => setActualStep(index)}
-                    />
-                    {index < steps.length - 1 && (
-                        <div className={clsx(
-                            "h-1 flex-1",
-                            index < actualStep ? "bg-blue-500" : "bg-gray-500",
-                        )}
-                        />
-                    )}
-                </div>
-            ))}
+        <div className="relative flex items-center flex-1">
+            <div className="flex items-center justify-between w-full">
+                {steps.map((step: JSX.Element, index: number) => (
+                    components[index]
+                ))}
+            </div>
+            <div
+                className="absolute bg-gray-100 border rounded-lg -z-10 px-4 py-4"
+                style={{
+                    left: `${Math.round(100 * slicePoint / steps.length)}%`,
+                    right: 0,
+                }}
+            />
         </div>
     )
 }
