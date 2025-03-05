fc0cd836d5cdd3da3a7b0432fd362eecd9e280b6
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-03-05 14:33:38 -0300

    :construction: :bug: Actualizando props de AtepSpectrumSegmentation

--- app/src/components/pages/StepSpectrumSegmentation.tsx
+++ app/src/components/pages/StepSpectrumSegmentation.tsx
@@ -4,13 +4,41 @@ import { useState } from "react"
 import { LoadFile } from "../molecules/LoadFile"
 import { SegmentationUI } from "../organisms/SegmentationUI"
 
-export function StepSpectrumSegmentation({ onComplete }: StepProps) {
-    const [file, setFile] = useState<string | null>(null)
+export function StepSpectrumSegmentation({ index, processInfo, setProcessInfo }: StepProps) {
+  const [file, setFile] = useState<string | null>(null)
 
-    return (
-        <div className="w-full p-6">
-            {!file && <LoadFile onSelect={(fileValue: string) => setFile(fileValue)} />}
-            {file && <SegmentationUI file={file} onComplete={onComplete} enableAutodetect />}
-        </div>
-    )
+  function onComplete() {
+    /// Marca el paso actual como completado y el que le sigue como
+    /// que necesita actualizaciones
+    setProcessInfo(prev => ({
+      ...prev,
+      general: prev.general.map((step, i) => ({
+        ...step,
+        state: index === i
+          ? "COMPLETE"
+          : (index + 1 === i
+              ? "NECESSARY_CHANGES"
+              : step.state),
+      })),
+      perSpectrum: prev.perSpectrum.map((step, i) => (
+        {
+          ...step,
+          states: boundingBoxes.map(_ => (
+            i === 0
+              ? "NECESSARY_CHANGES" as const
+              : "NOT_REACHED" as const
+          )),
+        }
+
+      )),
+    }))
+    setActualStep(index + 1)
+  }
+
+  return (
+    <div className="w-full p-6">
+      {!file && <LoadFile onSelect={(fileValue: string) => setFile(fileValue)} />}
+      {file && <SegmentationUI file={file} onComplete={onComplete} enableAutodetect />}
+    </div>
+  )
 }
