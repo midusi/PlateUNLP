fb153c91477ac6f31a65765a284957c426dd4b0a
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-03-06 12:02:30 -0300

    :sparkles: Actualizando StepCalibration con nuevo sistema de navegacion

--- app/src/components/pages/StepCalibration.tsx
+++ app/src/components/pages/StepCalibration.tsx
@@ -9,9 +9,49 @@ import { InferenceForm } from "@/components/organisms/InferenceForm"
 import { ReferenceLampForm } from "@/components/organisms/ReferenceLampForm"
 import { useState } from "react"
 import { Pane, ResizablePanes } from "resizable-panes-react"
+import { useGlobalStore } from "@/hooks/use-global-store"
 
-export function StepCalibration({ onComplete }: StepProps) {
+export function StepCalibration({ index, processInfo, setProcessInfo }: StepProps) {
     const [scienceSpectrumData, setScienceSpectrumData] = useState<EmpiricalSpectrumPoint[] | null>(null)
+    const [setActualStep, selectedSpectrum] = useGlobalStore(s => [
+        s.setActualStep,
+        s.selectedSpectrum
+    ])
+
+    function onComplete() {
+        /// AGREGAR GUARDADO DE DATOS EXTRAIDOS
+
+        /// Marca el paso actual como completado y el que le sigue como
+        /// que necesita actualizaciones
+        const generalTotal = processInfo.general.length
+        setProcessInfo(prev => ({
+            ...prev,
+            perSpectrum: prev.perSpectrum.map((step, i) => (
+                (i === (index - generalTotal)) // La etapa actual de selectedSpectrum se marca como completado
+                    ? {
+                        ...step,
+                        states: step.states!.map((state, j) => (
+                            j === selectedSpectrum
+                                ? "COMPLETE" as const
+                                : state
+                        )),
+                    }
+                    : ((i === (index - generalTotal + 1))// Si hay otra etapa adelante se la marca como que necesita cambios
+                        ? {
+                            ...step,
+                            states: step.states!.map((state, j) => (
+                                j === selectedSpectrum
+                                    ? "NECESSARY_CHANGES" as const
+                                    : state
+                            )),
+                        }
+                        : step // Cualquier otra etapa mantiene su informacion
+                    )
+            )),
+        }))
+
+        setActualStep(2) // Redirige a Spectrum Selection
+    }
 
     return (
         <>
