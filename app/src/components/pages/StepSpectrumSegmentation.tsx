46fc4f554ad2b751b6af6cc0e0078e8b6ea8a94a
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-03-06 10:27:09 -0300

    :bug: Logica de indexado erronea

--- app/src/components/pages/StepSpectrumSegmentation.tsx
+++ app/src/components/pages/StepSpectrumSegmentation.tsx
@@ -6,7 +6,7 @@ import { useState } from "react"
 import { LoadFile } from "../molecules/LoadFile"
 import { SegmentationUI } from "../organisms/SegmentationUI"
 
-export function StepSpectrumSegmentation({ index, setProcessInfo }: StepProps) {
+export function StepSpectrumSegmentation({ index, processInfo, setProcessInfo }: StepProps) {
   const [file, setFile] = useState<string | null>(null)
   const [boundingBoxes, setBoundingBoxes] = useState<BoundingBox[]>([])
   const [setActualStep, selectedSpectrum] = useGlobalStore(s => [
@@ -20,12 +20,12 @@ export function StepSpectrumSegmentation({ index, setProcessInfo }: StepProps) {
       spectrums: prev.spectrums.map((spectrum, index) => (
         index === selectedSpectrum
           ? {
-              ...spectrum,
-              images: {
-                lamps: croppedImages,
-                scienceSpectrum: croppedImages[0],
-              }, // FALTA: DISTINGUIR ENTRE LAMPARA Y ESPECTRO DE CIENCIA, AHORA SOLO LO ASIGNO AL AZAR.
-            }
+            ...spectrum,
+            images: {
+              lamps: croppedImages,
+              scienceSpectrum: croppedImages[0],
+            }, // FALTA: DISTINGUIR ENTRE LAMPARA Y ESPECTRO DE CIENCIA, AHORA SOLO LO ASIGNO AL AZAR.
+          }
           : spectrum
       )),
     }))
@@ -36,29 +36,31 @@ export function StepSpectrumSegmentation({ index, setProcessInfo }: StepProps) {
 
     /// Marca el paso actual como completado y el que le sigue como
     /// que necesita actualizaciones
+    const generalTotal = processInfo.general.length
+    console.log(selectedSpectrum, index)
     setProcessInfo(prev => ({
       ...prev,
       perSpectrum: prev.perSpectrum.map((step, i) => (
-        (i === index) // La etapa actual de selectedSpectrum se marca como completado
+        (i === (index - generalTotal)) // La etapa actual de selectedSpectrum se marca como completado
           ? {
+            ...step,
+            states: step.states!.map((state, j) => (
+              j === selectedSpectrum
+                ? "COMPLETE" as const
+                : state
+            )),
+          }
+          : ((i === (index - generalTotal + 1))// Si hay otra etapa adelante se la marca como que necesita cambios
+            ? {
               ...step,
               states: step.states!.map((state, j) => (
                 j === selectedSpectrum
-                  ? "COMPLETE" as const
+                  ? "NECESSARY_CHANGES" as const
                   : state
               )),
             }
-          : ((i === index + 1)// Si hay otra etapa adelante se la marca como que necesita cambios
-              ? {
-                  ...step,
-                  states: step.states!.map((state, j) => (
-                    j === selectedSpectrum
-                      ? "NECESSARY_CHANGES" as const
-                      : state
-                  )),
-                }
-              : step // Cualquier otra etapa mantiene su informacion
-            )
+            : step // Cualquier otra etapa mantiene su informacion
+          )
       )),
     }))
     setActualStep(index + 1)
