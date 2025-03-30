235eab68ce0b369dbbf52a3ea6453e9903a19bd0
Lautaro Pereyra <heelios12@gmail.com>
2025-03-30 09:00:11 -0300

    Se quito la logica de calcular los metadatos del front, y se agrego la consulta a la api.

--- app/src/lib/calculateMetadata/epoca.ts
+++
@@ -1,8 +0,0 @@
-import { getDayOfYear, getDaysInYear } from 'date-fns'
-
-
-export const epoca = (dateobs: Date): number => {
-    const days = getDaysInYear(dateobs) // 365 o 366
-    const year = dateobs.getFullYear() + (getDayOfYear(dateobs) - 1) / days
-    return parseFloat(year.toFixed(1)) // Devuelve el año con una decimal
-}
