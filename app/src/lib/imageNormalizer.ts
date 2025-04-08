1a9e2c65afaaaac4c39ec90f89faad92343d3348
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-04-08 20:27:35 -0300

    :recaycle: :sparkles: :memo: Documentado y extencion de funcionalidad de funcion para deteccion de color de fondo e inversion condicional de imagen

--- app/src/lib/imageNormalizer.ts
+++ app/src/lib/imageNormalizer.ts
@@ -1,95 +1,115 @@
-
 export async function align(imageb64: string): Promise<string> {
+  // Cargar imagen
+  const image = new Image()
+  image.src = imageb64
+  await new Promise((resolve) => {
+    image.onload = resolve
+  })
 
-    // Cargar imagen
-    const image = new Image()
-    image.src = imageb64
-    await new Promise((resolve) => {
-        image.onload = resolve
-    })
+  // alto & ancho
+  const { naturalWidth, naturalHeight } = image
 
-    // alto & ancho
-    const { naturalWidth, naturalHeight } = image
-
-    // Si es mas alta que ancha la gira 90º
-    let imageProcessed = imageb64
-    if (naturalHeight > naturalWidth) {
-        const canvas = document.createElement("canvas")
-        const ctx = canvas.getContext("2d")
-        if (!ctx) throw new Error('No se pudo obtener el contexto del canvas');
-
-        canvas.width = naturalHeight
-        canvas.height = naturalWidth
-
-        ctx.translate(naturalHeight, 0)
-        ctx.rotate(Math.PI / 2) // 90º
-
-        ctx.drawImage(image, 0, 0)
-
-        imageProcessed = canvas.toDataURL();
-    }
-
-    return imageProcessed
-}
-
-export async function ensureWhite(imageb64: string): Promise<string> {
-
-    const image = new Image()
-    image.src = imageb64
-    await new Promise((resolve, reject) => {
-        image.onload = resolve
-        image.onerror = reject
-    })
-
-    // Detectar color fondo (Blanco | Negro)
+  // Si es mas alta que ancha la gira 90º
+  let imageProcessed = imageb64
+  if (naturalHeight > naturalWidth) {
     const canvas = document.createElement("canvas")
     const ctx = canvas.getContext("2d")
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
+    if (!ctx)
+      throw new Error("No se pudo obtener el contexto del canvas")
 
-    if (!ctx) throw new Error('No se pudo obtener el contexto del canvas');
+    canvas.width = naturalHeight
+    canvas.height = naturalWidth
+
+    ctx.translate(naturalHeight, 0)
+    ctx.rotate(Math.PI / 2) // 90º
 
     ctx.drawImage(image, 0, 0)
 
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-    const data = imageData.data
+    imageProcessed = canvas.toDataURL()
+  }
 
-    let brightnessSum = 0
+  return imageProcessed
+}
 
-    // Fórmula de luminancia (luma)
-    const getBrightness = (r: number, g: number, b: number) =>
-        0.299 * r + 0.587 * g + 0.114 * b;
+/**
+ * Revisa que el fondo de una funcion sea blanco si es negro invierte los colores.
+ * @param {string} imageb64 - La imagen a revisar.
+ * @param {"white" | "black"} targetColor - Color objetivo
+ * @returns {Promise<{ image:string, bgColor: "white" | "black" }>} -
+ * Una promesa que contiene la imagen procesada y un string indicando el color que quedo de fondo.
+ */
+export async function ensureWhite(
+  imageb64: string,
+  targetColor: "white" | "black",
+): Promise<{ image: string, bgColor: "white" | "black" }> {
+  const image = new Image()
+  image.src = imageb64
+  await new Promise((resolve, reject) => {
+    image.onload = resolve
+    image.onerror = reject
+  })
 
-    const width = canvas.width;
-    const height = canvas.height;
+  // Detectar color fondo (Blanco | Negro)
+  const canvas = document.createElement("canvas")
+  const ctx = canvas.getContext("2d")
+  canvas.width = image.naturalWidth
+  canvas.height = image.naturalHeight
 
+  if (!ctx)
+    throw new Error("No se pudo obtener el contexto del canvas")
+
+  ctx.drawImage(image, 0, 0)
+
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  const data = imageData.data
+
+  let brightnessSum = 0
+
+  // Fórmula de luminancia (luma)
+  const getBrightness = (r: number, g: number, b: number) =>
+    0.299 * r + 0.587 * g + 0.114 * b
+
+  const width = canvas.width
+  const height = canvas.height
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      const idx = (i * width + j) * 4
+      const r = data[idx]
+      const g = data[idx + 1]
+      const b = data[idx + 2]
+      brightnessSum += getBrightness(r, g, b)
+    }
+  }
+
+  const avgBrightness = brightnessSum / (height * width)
+
+  // Variables de retorno
+  let imageProcessed = imageb64
+
+  // Color predominante actual
+  let mainColor: "white" | "black"
+  if (avgBrightness < 128) {
+    mainColor = "black"
+  }
+  else {
+    mainColor = "white"
+  }
+
+  // Si color predominante es distinto de objetivo lo invierte
+  if (mainColor !== targetColor) {
     for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            const idx = (i * width + j) * 4
-            const r = data[idx]
-            const g = data[idx + 1]
-            const b = data[idx + 2]
-            brightnessSum += getBrightness(r, g, b)
-        }
+      for (let j = 0; j < width; j++) {
+        const idx = (i * width + j) * 4
+        data[idx] = 255 - data[idx]
+        data[idx + 1] = 255 - data[idx + 1]
+        data[idx + 2] = 255 - data[idx + 2]
+      }
     }
+    ctx.putImageData(imageData, 0, 0)
 
-    const avgBrightness = brightnessSum / (height * width)
+    imageProcessed = canvas.toDataURL()
+  }
 
-    // Si el fondo es negro lo invertimos a blanco
-    let imageProcessed = imageb64
-    if (avgBrightness < 128) {
-        for (let i = 0; i < height; i++) {
-            for (let j = 0; j < width; j++) {
-                const idx = (i * width + j) * 4
-                data[idx] = 255 - data[idx]
-                data[idx + 1] = 255 - data[idx + 1]
-                data[idx + 2] = 255 - data[idx + 2]
-            }
-        }
-        ctx.putImageData(imageData, 0, 0)
-
-        imageProcessed = canvas.toDataURL();
-    }
-
-    return imageProcessed
+  return { image: imageProcessed, bgColor: targetColor }
 }
