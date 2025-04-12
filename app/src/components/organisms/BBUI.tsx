7f639a1e000c3a0edcc709d2ee71b06d3731c7c9
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-04-12 18:14:38 -0300

    :recycle: Modularization of componente

--- app/src/components/organisms/BBUI.tsx
+++ app/src/components/organisms/BBUI.tsx
@@ -1,6 +1,8 @@
+import type { Dispatch, SetStateAction } from "react"
 import clsx from "clsx"
 import { Palette, RotateCw } from "lucide-react"
 import { useState } from "react"
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch"
 import { Button } from "../atoms/button"
 import { Card } from "../atoms/card"
 
@@ -12,17 +14,6 @@ export function BBUI() {
   const [image, setImage] = useState<null | string> (null)
   const [bgWhite, setBgWhite] = useState(true)
   const [rotation, setRotation] = useState(0)
-
-  function handleImageUpload(e: React.ChangeEvent<HTMLInputElement>) {
-    const file = e.target.files?.[0]
-    if (file) {
-      const reader = new FileReader()
-      reader.onload = (e) => {
-        setImage(e.target?.result as string)
-      }
-      reader.readAsDataURL(file)
-    }
-  }
 
   return (
     <>
@@ -60,33 +51,14 @@ export function BBUI() {
           </div>
         </div>
         <div className={clsx(
-          "p-6 w-full h-[500px]",
+          "w-full h-[500px]",
           "flex items-center justify-center",
-          " bg-slate-50 rounded-lg overflow-hidden",
+          " bg-slate-50 overflow-hidden",
         )}
         >
           {image
-            ? <span>Imagen cargada</span>
-            : (
-                <label
-                  htmlFor="image-upload"
-                  className="flex flex-col items-center justify-center w-full h-full border-2 border-dashed rounded-lg cursor-pointer bg-slate-50 hover:bg-slate-100"
-                >
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <p className="mb-2 text-sm text-slate-500">
-                      <span className="font-semibold">Click to upload an image</span>
-                    </p>
-                    <p className="text-xs text-slate-500">PNG, JPG or TIFF</p>
-                  </div>
-                  <input
-                    id="image-upload"
-                    type="file"
-                    className="hidden"
-                    accept="image/*"
-                    onChange={handleImageUpload}
-                  />
-                </label>
-              )}
+            ? <ImageViewer image={image} />
+            : <ImageLoader setImage={setImage} />}
         </div>
       </Card>
       <div className="flex justify-center pt-4">
@@ -98,5 +70,72 @@ export function BBUI() {
         </Button>
       </div>
     </>
+  )
+}
+
+function ImageViewer({ image }: { image: string }) {
+  return (
+    <div
+      className="bg-green-100 w-full h-full"
+      style={{ width: "100%", height: "100%" }}
+    >
+      <TransformWrapper
+        centerOnInit
+        initialScale={1}
+        minScale={0.25}
+        doubleClick={{ step: 0.7 }}
+      >
+        <TransformComponent>
+          <img
+            className="bg-blue-500 border-black"
+            style={{
+              objectFit: "contain",
+            }}
+            src={image}
+            alt="Bounding Box Editor"
+          />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  )
+}
+
+interface ImageLoaderProps {
+  setImage: Dispatch<SetStateAction<string | null>>
+}
+
+function ImageLoader({ setImage }: ImageLoaderProps) {
+  function handleImageUpload(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0]
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        setImage(e.target?.result as string)
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  return (
+    <div className="w-full h-full p-6 rounded-lg">
+      <label
+        htmlFor="image-upload"
+        className="flex flex-col items-center justify-center w-full h-full border-2 border-dashed rounded-lg cursor-pointer bg-slate-50 hover:bg-slate-100"
+      >
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+          <p className="mb-2 text-sm text-slate-500">
+            <span className="font-semibold">Click to upload an image</span>
+          </p>
+          <p className="text-xs text-slate-500">PNG, JPG or TIFF</p>
+        </div>
+        <input
+          id="image-upload"
+          type="file"
+          className="hidden"
+          accept="image/*"
+          onChange={handleImageUpload}
+        />
+      </label>
+    </div>
   )
 }
