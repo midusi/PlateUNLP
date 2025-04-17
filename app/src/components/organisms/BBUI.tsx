cc5846f976e7d8f2a02256f049823859d8a27458
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-04-17 17:53:33 -0300

    :wrench: :bug: BugFix deshabilitado de arrastre de imagen durante dibujado de caja

--- app/src/components/organisms/BBUI.tsx
+++ app/src/components/organisms/BBUI.tsx
@@ -239,6 +239,7 @@ function ImageViewer({
           maxScale={imageScale * zoomFactor}
           centerOnInit
           doubleClick={{ step: 0.7 }}
+          disablePadding={isDrawingMode}
         >
           <TransformComponent
             wrapperStyle={{
