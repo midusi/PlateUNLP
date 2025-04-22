373c025eeb09ec86d04c7057adc7e6f640d34881
JuanM04 <me@juanm04.com>
2025-04-22 00:05:13 -0300

    feat: 🗃️ SIMBAD

--- app/src/services/calculateMetadata/index.ts
+++
@@ -1,8 +0,0 @@
-import apiClient from "../api"
-import { IApiResponse } from "../api/types"
-import { IMetadataResponse } from "./types"
-
-
-export const getMetadata = async (requiredData: { observat: string, object: string, dateObs: Date, ut: number }): Promise<IApiResponse<IMetadataResponse>> => {
-    return (await apiClient.get<IApiResponse<IMetadataResponse>>(`observat=${requiredData.observat}/object=${requiredData.object}/dateobs=${requiredData.dateObs}/ut=${requiredData.ut}`)).data
-}
