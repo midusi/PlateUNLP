784b16c5fcc27b157e65e31fe56eab4bce8e57f8
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-04-23 12:13:38 -0300

    :rotating_ligth: Typescript warning

--- app/src/components/pages/StepFeatureExtraction.tsx
+++ app/src/components/pages/StepFeatureExtraction.tsx
@@ -1,10 +1,10 @@
 import type { Point } from "@/interfaces/Point"
 import type { StepProps } from "@/interfaces/StepProps"
 import { useGlobalStore } from "@/hooks/use-global-store"
-import { AnimatedAxis, AnimatedGrid, AnimatedLineSeries, darkTheme, XYChart } from "@visx/xychart"
-import { Button } from "../atoms/button"
 import { findXspacedPoints, matrixToUrl, obtainimageMatrix, obtainImageSegments } from "@/lib/image"
+import { AnimatedAxis, AnimatedGrid, AnimatedLineSeries, darkTheme, XYChart } from "@visx/xychart"
 import { useEffect, useState } from "react"
+import { Button } from "../atoms/button"
 
 export function StepFeatureExtraction({ index, processInfo, setProcessInfo }: StepProps) {
   const imageSrc = "/forTest/Lamp1.png"
@@ -14,23 +14,24 @@ export function StepFeatureExtraction({ index, processInfo, setProcessInfo }: St
   ])
   const [imageData, setImageData] = useState<Uint8ClampedArray<ArrayBufferLike> | null>(null)
   const [segmentsData, setSegmentsData] = useState<{
-    data: Uint8ClampedArray<ArrayBufferLike>[],
-    width: number,
+    data: Uint8ClampedArray<ArrayBufferLike>[]
+    width: number
     height: number
   } | null>(null)
   useEffect(() => {
     obtainimageMatrix(imageSrc, true).then(({ data, width, height }) => {
-      if (!data) return
+      if (!data)
+        return
       setImageData(data)
       const points = findXspacedPoints(width, 3)
       const segmentWidth = 10
       const segmentsData = obtainImageSegments(data, width, height, points, segmentWidth)
       for (let sd in segmentsData) {
         if (sd.length !== segmentWidth * height * 4)
-          console.warn("Segment size mismatch:", sd.length, segmentWidth * height * 4);
+          console.warn("Segment size mismatch:", sd.length, segmentWidth * height * 4)
       }
       setSegmentsData({ data: segmentsData, width: segmentWidth, height })
-    }).catch(err => {
+    }).catch((err) => {
       console.error("Error loading Image Data:", err)
     })
   }, [imageSrc])
@@ -48,24 +49,24 @@ export function StepFeatureExtraction({ index, processInfo, setProcessInfo }: St
         specificSteps: prev.processingStatus.specificSteps.map((step, i) => (
           (i === (index - generalTotal)) // La etapa actual de selectedSpectrum se marca como completado
             ? {
-              ...step,
-              states: step.states!.map((state, j) => (
-                j === selectedSpectrum
-                  ? "COMPLETE" as const
-                  : state
-              )),
-            }
-            : ((i === (index - generalTotal + 1))// Si hay otra etapa adelante se la marca como que necesita cambios
-              ? {
                 ...step,
                 states: step.states!.map((state, j) => (
                   j === selectedSpectrum
-                    ? "NECESSARY_CHANGES" as const
+                    ? "COMPLETE" as const
                     : state
                 )),
               }
-              : step // Cualquier otra etapa mantiene su informacion
-            )
+            : ((i === (index - generalTotal + 1))// Si hay otra etapa adelante se la marca como que necesita cambios
+                ? {
+                    ...step,
+                    states: step.states!.map((state, j) => (
+                      j === selectedSpectrum
+                        ? "NECESSARY_CHANGES" as const
+                        : state
+                    )),
+                  }
+                : step // Cualquier otra etapa mantiene su informacion
+              )
         )),
       },
     }))
@@ -75,17 +76,20 @@ export function StepFeatureExtraction({ index, processInfo, setProcessInfo }: St
   return (
     <div className="w-full p-6 flex flex-col items-center">
       <SimpleImage src={imageSrc} />
-      {segmentsData && <div className="p-2 bg-slate-200">
-        {segmentsData.data.map(data => (
-          <div key={data[0]} className="p-2 m-2 bg-red-300">
-            <SimpleImage src={matrixToUrl(
-              data,
-              segmentsData.width,
-              segmentsData.height
-            )} />
-          </div>
-        ))}
-      </div>}
+      {segmentsData && (
+        <div className="p-2 bg-slate-200">
+          {segmentsData.data.map(data => (
+            <div key={data[0]} className="p-2 m-2 bg-red-300">
+              <SimpleImage src={matrixToUrl(
+                data,
+                segmentsData.width,
+                segmentsData.height,
+              )}
+              />
+            </div>
+          ))}
+        </div>
+      )}
       <SimpleFunctionXY />
       <hr className="w-full mb-4"></hr>
       <Button onClick={() => onComplete()}>
