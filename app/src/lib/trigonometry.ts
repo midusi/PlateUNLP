b6b483d6b776d83935a03ba4e3d0a263a62eb44f
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-05-02 11:38:35 -0300

    :bug: :sparkles: La funcion contempla que la pendiente sea totalmente vertical.

--- app/src/lib/trigonometry.ts
+++ app/src/lib/trigonometry.ts
@@ -10,17 +10,25 @@ import type { Point } from "@/interfaces/Point"
  * Puntos recorriendo la distancia hacia delante y hacia atrás
  */
 export function extremePoints(point: Point, m: number, distance: number): { forward: Point, backward: Point } {
-  // Datos conocidos
-  const hipotenusa = distance
-  const anguloDeM = Math.atan(m) * (180 / Math.PI)
-  // Cateto opuesto
-  const opuesto = Math.sin(anguloDeM) * hipotenusa
-  // Cateto adyacente
-  const adyacente = Math.cos(anguloDeM) * hipotenusa
-  // Cestino sumando
-  const forward = { x: point.x + adyacente, y: point.y + opuesto }
-  // Cestino restando
-  const backward = { x: point.x - adyacente, y: point.y - opuesto }
+  let forward: Point
+  let backward: Point
+  if (m === Infinity) {
+    forward = { x: point.x, y: point.y + distance }
+    backward = { x: point.x, y: point.y - distance }
+  }
+  else {
+    // Datos conocidos
+    const hipotenusa = distance
+    const anguloDeM = Math.atan(m) * (180 / Math.PI)
+    // Cateto opuesto
+    const opuesto = Math.sin(anguloDeM) * hipotenusa
+    // Cateto adyacente
+    const adyacente = Math.cos(anguloDeM) * hipotenusa
+    // Cestino sumando
+    forward = { x: point.x + adyacente, y: point.y + opuesto }
+    // Cestino restando
+    backward = { x: point.x - adyacente, y: point.y - opuesto }
+  }
 
   return { forward, backward }
 }
