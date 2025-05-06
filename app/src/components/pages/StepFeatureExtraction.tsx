83b73a16ff185042589a595955f8432dd116e1a0
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-05-06 12:53:42 -0300

    :lipstick: Visualizar apaertura ahora con lineas punteadas.

--- app/src/components/pages/StepFeatureExtraction.tsx
+++ app/src/components/pages/StepFeatureExtraction.tsx
@@ -26,38 +26,38 @@ export function StepFeatureExtraction({ index, processInfo, setProcessInfo }: St
   ])
 
 
-  // const [urls, setUrls] = useState<{ science: string, lamp1: string, lamp2: string } | null>({
-  //   science: "/forTest/Test1_Science1.png",
-  //   lamp1: "/forTest/Test1_Lamp1.png",
-  //   lamp2: "/forTest/Test1_Lamp2.png"
-  // })
-  const [urls, setUrls] = useState<{ science: string, lamp1: string, lamp2: string } | null>(null)
-  useEffect(() => {
-    const boundingBoxes: BoundingBox[] = [
-      processInfo.data.spectrums[selectedSpectrum!].parts.science.boundingBox!,
-      processInfo.data.spectrums[selectedSpectrum!].parts.lamp1.boundingBox!,
-      processInfo.data.spectrums[selectedSpectrum!].parts.lamp2.boundingBox!,
-    ].map(bb => ({
-      ...bb,
-      x: bb.x + processInfo.data.spectrums[selectedSpectrum!].spectrumBoundingBox.x,
-      y: bb.y + processInfo.data.spectrums[selectedSpectrum!].spectrumBoundingBox.y
-    }))
+  const [urls, setUrls] = useState<{ science: string, lamp1: string, lamp2: string } | null>({
+    science: "/forTest/Test1_Science1.png",
+    lamp1: "/forTest/Test1_Lamp1.png",
+    lamp2: "/forTest/Test1_Lamp2.png"
+  })
+  // const [urls, setUrls] = useState<{ science: string, lamp1: string, lamp2: string } | null>(null)
+  // useEffect(() => {
+  //   const boundingBoxes: BoundingBox[] = [
+  //     processInfo.data.spectrums[selectedSpectrum!].parts.science.boundingBox!,
+  //     processInfo.data.spectrums[selectedSpectrum!].parts.lamp1.boundingBox!,
+  //     processInfo.data.spectrums[selectedSpectrum!].parts.lamp2.boundingBox!,
+  //   ].map(bb => ({
+  //     ...bb,
+  //     x: bb.x + processInfo.data.spectrums[selectedSpectrum!].spectrumBoundingBox.x,
+  //     y: bb.y + processInfo.data.spectrums[selectedSpectrum!].spectrumBoundingBox.y
+  //   }))
 
-    cropImages(
-      processInfo.data.plate.scanImage!,
-      boundingBoxes,
-    ).then(([science, lamp1, lamp2]) => {
-      // console.log("BB: ", boundingBoxes)
-      // for (let i = 0; i < [science, lamp1, lamp2].length; i++) {
-      //   const a = document.createElement("a")
-      //   a.href = [science, lamp1, lamp2][i]
-      //   a.download = "imagen.png"
-      //   a.click()
-      // }
+  //   cropImages(
+  //     processInfo.data.plate.scanImage!,
+  //     boundingBoxes,
+  //   ).then(([science, lamp1, lamp2]) => {
+  //     // console.log("BB: ", boundingBoxes)
+  //     // for (let i = 0; i < [science, lamp1, lamp2].length; i++) {
+  //     //   const a = document.createElement("a")
+  //     //   a.href = [science, lamp1, lamp2][i]
+  //     //   a.download = "imagen.png"
+  //     //   a.click()
+  //     // }
 
-      setUrls({ science, lamp1, lamp2 })
-    })
-  }, [processInfo.data.plate.scanImage, processInfo.data.spectrums, selectedSpectrum])
+  //     setUrls({ science, lamp1, lamp2 })
+  //   })
+  // }, [processInfo.data.plate.scanImage, processInfo.data.spectrums, selectedSpectrum])
 
   const {
     scienceInfo,
