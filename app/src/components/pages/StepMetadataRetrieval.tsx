51a986d4bef675d17fb1fef46d42f1df121ec749
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-05-23 11:34:56 -0300

    :rotating_ligth: Wargning por posible null

--- app/src/components/pages/StepMetadataRetrieval.tsx
+++ app/src/components/pages/StepMetadataRetrieval.tsx
@@ -25,24 +25,24 @@ export function StepMetadataRetrieval({ index, processInfo, setProcessInfo }: St
         specificSteps: prev.processingStatus.specificSteps.map((step, i) => (
           (i === (index - generalTotal)) // La etapa actual de selectedSpectrum se marca como completado
             ? {
+              ...step,
+              states: step.states!.map((state, j) => (
+                j === selectedSpectrum
+                  ? "COMPLETE" as const
+                  : state
+              )),
+            }
+            : ((i === (index - generalTotal + 1))// Si hay otra etapa adelante se la marca como que necesita cambios
+              ? {
                 ...step,
                 states: step.states!.map((state, j) => (
                   j === selectedSpectrum
-                    ? "COMPLETE" as const
+                    ? "NECESSARY_CHANGES" as const
                     : state
                 )),
               }
-            : ((i === (index - generalTotal + 1))// Si hay otra etapa adelante se la marca como que necesita cambios
-                ? {
-                    ...step,
-                    states: step.states!.map((state, j) => (
-                      j === selectedSpectrum
-                        ? "NECESSARY_CHANGES" as const
-                        : state
-                    )),
-                  }
-                : step // Cualquier otra etapa mantiene su informacion
-              )
+              : step // Cualquier otra etapa mantiene su informacion
+            )
         )),
       },
     }))
@@ -57,9 +57,9 @@ export function StepMetadataRetrieval({ index, processInfo, setProcessInfo }: St
 
         <div className="flex flex-col w-full flex justify-center mb-6">
           <BoxMetadataReadOnly
-            OBJECT={processInfo.data.spectrums[selectedSpectrum].metadata.OBJECT}
-            DATE_OBS={processInfo.data.spectrums[selectedSpectrum].metadata.DATE_OBS}
-            UT={processInfo.data.spectrums[selectedSpectrum].metadata.UT}
+            OBJECT={processInfo.data.spectrums[selectedSpectrum!].metadata.OBJECT}
+            DATE_OBS={processInfo.data.spectrums[selectedSpectrum!].metadata.DATE_OBS}
+            UT={processInfo.data.spectrums[selectedSpectrum!].metadata.UT}
           />
           <hr className="my-4" />
           <SpectrumMetadataForm ref={spectrumMetadataFormRef} />
