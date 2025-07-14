8233bc17feb092f678784f0679c15db55916836d
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-07-14 12:50:23 -0300

    🔧 minimal Zoom adjust

--- app/components/BoundingBoxer.tsx
+++ app/components/BoundingBoxer.tsx
@@ -114,7 +114,7 @@ export function BoundingBoxer({
       {imageScale ? (
         <TransformWrapper
           initialScale={imageScale}
-          minScale={imageScale}
+          minScale={imageScale * 0.5}
           centerOnInit
           centerZoomedOut
         >
