c8f558a50350fd3dc8e890fbba4bfcfb3bbd7d8a
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-07-17 19:16:06 -0300

    :zap: Espectros 1D funcional

--- app/components/ImageWithPixelExtraction.tsx
+++ app/components/ImageWithPixelExtraction.tsx
@@ -1,11 +1,11 @@
 import clsx from "clsx"
-import { useEffect, useRef } from "react"
+import { useEffect, useMemo, useRef } from "react"
 import type { Point } from "~/types/Point"
 
 interface ImageWithPixelExtractionProps {
   title?: string
   /** Imagen */
-  src: string
+  image: string | Uint8Array
   imageAlt?: string
   pointsWMed?: Point[]
   drawFunction: (x: number) => number
@@ -18,12 +18,17 @@ interface ImageWithPixelExtractionProps {
 
 export function ImageWithPixelExtraction({
   title,
-  src,
+  image,
   imageAlt,
   pointsWMed,
   drawFunction,
   opening,
 }: ImageWithPixelExtractionProps) {
+  const src = useMemo(
+    () => (typeof image === "string" ? image : URL.createObjectURL(new Blob([image]))),
+    [image],
+  )
+
   return (
     <div className={clsx("relative flex w-full items-center justify-center ", "px-16", "pt-2")}>
       <div className="flex flex-col">
--- app/routes/_app/observation.$observationId/-components/SpectrumsFeatures.tsx
+++ app/routes/_app/observation.$observationId/-components/SpectrumsFeatures.tsx
@@ -1,286 +1,193 @@
-import { useEffect, useState } from "react"
-import { Card, CardContent } from "~/components/ui/card"
+import { useEffect, useState } from "react";
+import { Card, CardContent } from "~/components/ui/card";
 import {
-  type extractFeaturesResponse,
-  extractLamp,
-  extractScience,
-  type extractSpectrumResponse,
-} from "~/lib/extract-features"
-import { crop, loadImage, obtainimageMatrix } from "~/lib/image"
-import { notifyError } from "~/lib/notifications"
-import { cn } from "~/lib/utils"
-import { ImageWithPixelExtraction } from "../../../../components/ImageWithPixelExtraction"
-import { SimpleFunctionXY } from "../../../../components/SimpleFunctionXY"
-import type { getSpectrums } from "../-actions/get-spectrums"
-import type { Spectrum } from "../-utils/spectrum-to-bounding-box"
+	extractLamp,
+	extractScience,
+	type extractSpectrumResponse,
+} from "~/lib/extract-features";
+import { loadImage } from "~/lib/image";
+import { notifyError } from "~/lib/notifications";
+import { cn } from "~/lib/utils";
+import { ImageWithPixelExtraction } from "../../../../components/ImageWithPixelExtraction";
+import { SimpleFunctionXY } from "../../../../components/SimpleFunctionXY";
+import type { getSpectrums } from "../-actions/get-spectrums";
+import type { Spectrum } from "../-utils/spectrum-to-bounding-box";
 
 export function SpectrumsFeatures({
-  observationId,
-  spectrums,
+	observationId,
+	spectrums,
 }: {
-  observationId: string
-  spectrums: Awaited<ReturnType<typeof getSpectrums>>
+	observationId: string;
+	spectrums: Awaited<ReturnType<typeof getSpectrums>>;
 }) {
-  const countCheckpoints = 5
-  const segmentWidth = 60
-  const useSpline = false
-  const reuseScienceFunction = true
+	const countCheckpoints = 5;
+	const segmentWidth = 60;
+	const useSpline = false;
+	const reuseScienceFunction = true;
 
-  const [science, setScience] = useState<Uint8Array>()
-  const [lamp1, setLamp1] = useState<Uint8Array>()
-  const [lamp2, setLamp2] = useState<Uint8Array>()
-  const [scienceAnalysis, setScienceAnalysis] = useState<extractSpectrumResponse>()
-  const [lamp1Analysis, setLamp1Analysis] = useState<extractSpectrumResponse>()
-  const [specAnalysis, setSpecAnalysis] =
-    useState<extractFeaturesResponse<Uint8ClampedArray<ArrayBufferLike>>>()
-  const [state, setState] = useState<"waiting" | "running" | "ready">("waiting")
+	const [scienceAnalysis, setScienceAnalysis] = useState<{
+		width: number;
+		height: number;
+		analysis: extractSpectrumResponse;
+	}>();
+	const [state, setState] = useState<"waiting" | "running" | "ready">(
+		"waiting",
+	);
 
-  const [observationImage, setObservationImage] = useState<HTMLImageElement | null>(null)
+	const [observationImage, setObservationImage] =
+		useState<HTMLImageElement | null>(null);
 
-  // Crop and save the spectrum image to be analyzed
-  const [spectrumsData, setSpectrumsData] = useState<(Spectrum & { data: Uint8Array })[]>([])
-  useEffect(() => {
-    if (!observationImage) {
-      loadImage(`/observation/${observationId}/image`).then((image) => setObservationImage(image))
-      return
-    }
+	// Crop and save the spectrum image to be analyzed
+	const [spectrumsData, setSpectrumsData] = useState<
+		{
+			data: Spectrum;
+			image: Uint8Array;
+			analysis: extractSpectrumResponse;
+		}[]
+	>([]);
+	useEffect(() => {
+		if (!observationImage) {
+			loadImage(`/observation/${observationId}/image`).then((image) =>
+				setObservationImage(image),
+			);
+			return;
+		}
+		if (spectrums.length === 0) return;
 
-    let specOk = false
-    let scienceResult: extractSpectrumResponse
-    let scienceWidht: number
-    let scienceHeigth: number
-    const indexSpectrum = 2
-    const specScience = spectrums[indexSpectrum]
-    spectrums[indexSpectrum].type = "science"
-    const spectrumsArr = [...spectrums]
-    spectrumsArr.splice(indexSpectrum, 1)
-    spectrumsArr.unshift(specScience)
+		/** Coloca primero el espectro de ciencia */
+		const indexSpectrum = spectrums.findIndex((s) => s.type === "science");
+		console.log(spectrums);
+		console.log(indexSpectrum);
+		const specScience = spectrums[indexSpectrum];
+		console.log(indexSpectrum);
+		spectrums[indexSpectrum].type = "science";
+		const spectrumsArr = [...spectrums];
+		spectrumsArr.splice(indexSpectrum, 1);
+		spectrumsArr.unshift(specScience);
+		let scienceInfo:
+			| {
+					width: number;
+					height: number;
+					analysis: extractSpectrumResponse;
+			  }
+			| undefined = scienceAnalysis && scienceAnalysis;
 
-    for (const spectrum of spectrumsArr) {
-      const saved = spectrumsData.find((s) => s.id === spectrum.id)
-      if (
-        saved &&
-        spectrum.type === saved.type &&
-        spectrum.imgTop === saved.imgTop &&
-        spectrum.imgLeft === saved.imgLeft &&
-        spectrum.imgWidth === saved.imgWidth &&
-        spectrum.imgHeight === saved.imgHeight
-      ) {
-        continue
-      }
+		for (const spectrum of spectrumsArr) {
+			const saved = spectrumsData.find((s) => s.data.id === spectrum.id);
+			if (
+				saved &&
+				spectrum.type === saved.data.type &&
+				spectrum.imgTop === saved.data.imgTop &&
+				spectrum.imgLeft === saved.data.imgLeft &&
+				spectrum.imgWidth === saved.data.imgWidth &&
+				spectrum.imgHeight === saved.data.imgHeight
+			) {
+				continue;
+			}
 
-      const canvas = document.createElement("canvas")
-      canvas.width = spectrum.imgWidth
-      canvas.height = spectrum.imgHeight
-      const ctx = canvas.getContext("2d")
-      if (!ctx) {
-        notifyError("Failed to create canvas context for spectrum image.")
-        return
-      }
-      ctx.filter = "grayscale(1)"
-      ctx.drawImage(
-        observationImage,
-        spectrum.imgLeft,
-        spectrum.imgTop,
-        spectrum.imgWidth,
-        spectrum.imgHeight,
-        0,
-        0,
-        spectrum.imgWidth,
-        spectrum.imgHeight,
-      )
-      const data = new Uint8Array(
-        ctx.getImageData(0, 0, spectrum.imgWidth, spectrum.imgHeight, {}).data.buffer,
-      )
-      canvas.remove()
-      setSpectrumsData((prev) =>
-        [...prev.filter((s) => s.id !== spectrum.id), { ...spectrum, data }].sort(
-          (a, b) => a.imgTop - b.imgTop || a.imgLeft - b.imgLeft,
-        ),
-      )
-      if (spectrum.type === "science") {
-        setScience(data)
-        const result = extractScience({
-          science: data,
-          width: spectrum.imgWidth,
-          height: spectrum.imgHeight,
-          countCheckpoints,
-          segmentWidth: segmentWidth,
-          fitFunction: "linal-regression",
-        })
-        scienceResult = result
-        scienceWidht = spectrum.imgWidth
-        scienceHeigth = spectrum.imgHeight
-        setScienceAnalysis(result)
-        specOk = true
-      } else if (specOk) {
-        setLamp1(data)
-        const result = extractLamp({
-          science: {
-            width: scienceWidht!,
-            height: scienceHeigth!,
-            mediasPoints: scienceResult!.mediasPoints,
-            opening: scienceResult!.opening,
-            rectFunction: scienceResult!.rectFunction,
-            transversalAvgs: scienceResult!.transversalAvgs,
-          },
-          lamp: data,
-          width: spectrum.imgWidth,
-          height: spectrum.imgHeight,
-          countCheckpoints,
-          segmentWidth: segmentWidth,
-          fitFunction: "linal-regression",
-        })
-        setLamp1Analysis(result)
-      }
-      console.log(specOk)
-    }
-    setState("ready")
-  }, [observationId, observationImage, spectrums, spectrumsData])
+			const canvas = document.createElement("canvas");
+			canvas.width = spectrum.imgWidth;
+			canvas.height = spectrum.imgHeight;
+			const ctx = canvas.getContext("2d");
+			if (!ctx) {
+				notifyError("Failed to create canvas context for spectrum image.");
+				return;
+			}
+			ctx.filter = "grayscale(1)";
+			ctx.drawImage(
+				observationImage,
+				spectrum.imgLeft,
+				spectrum.imgTop,
+				spectrum.imgWidth,
+				spectrum.imgHeight,
+				0,
+				0,
+				spectrum.imgWidth,
+				spectrum.imgHeight,
+			);
+			const data = new Uint8Array(
+				ctx.getImageData(0, 0, spectrum.imgWidth, spectrum.imgHeight, {}).data
+					.buffer,
+			);
+			canvas.remove();
 
-  //  useEffect(() => {
-  //    /** Si no hay suficientes espectros o el id de observacion no hace nada */
-  //    if (!observationId || spectrums.length < 3) {
-  //      setState("waiting")
-  //      return
-  //    }
-  //    setState("running")
-  //    const run = async () => {
-  //      /** Imagen de espectro de ciencia */
-  //      const science = await obtainimageMatrix(`/spectrum/${spectrums[0].id}/image`)
-  //      setScience(science.data)
-  //      /** Imagen de espectro de lampara de comparación 1 */
-  //      const lamp1 = await obtainimageMatrix(`/spectrum/${spectrums[1].id}/image`)
-  //      setLamp1(lamp1.data)
-  //      /** Imagen de espectro de lampara de comparación 2 */
-  //      const lamp2 = await obtainimageMatrix(`/spectrum/${spectrums[2].id}/image`)
-  //      setLamp2(lamp2.data)
-  //
-  //      /** Extraer caracteristicas de ciencia
-  //       * [specrum[0]] -> science
-  //       */
-  //      /** Extraer caracteristicas de lamp1
-  //       * [spectrum[1], science] -> lamp1
-  //       */
-  //
-  //      /** Extraer caracteristicas de lamp2
-  //       * [spectrum[2], science] -> lamp2
-  //       */
-  //
-  //      /** Extraer caracteristicas en base a las imagenes de los espectros. */
-  //      const spectrumAnalysis = extractFeatures(
-  //        countCheckpoints,
-  //        segmentWidth,
-  //        science,
-  //        lamp1,
-  //        lamp2,
-  //        useSpline,
-  //        reuseScienceFunction,
-  //      )
-  //
-  //      /** Actualizar variables superiores */
-  //      setSpecAnalysis(spectrumAnalysis)
-  //      setState("ready")
-  //    }
-  //
-  //    run()
-  //
-  //    /** Funcion de Limpieza */
-  //    return () => {}
-  //  }, [observationId, spectrums])
+			/** Extraer caracteristicas */
+			let result: extractSpectrumResponse;
+			if (spectrum.type === "science") {
+				result = extractScience({
+					science: data,
+					width: spectrum.imgWidth,
+					height: spectrum.imgHeight,
+					countCheckpoints,
+					segmentWidth: segmentWidth,
+					fitFunction: "linal-regression",
+				});
+				scienceInfo = {
+					width: spectrum.imgWidth,
+					height: spectrum.imgHeight,
+					analysis: result,
+				};
+				setScienceAnalysis({ ...scienceInfo });
+			} else {
+				result = extractLamp({
+					science: {
+						width: scienceInfo!.width,
+						height: scienceInfo!.height!,
+						mediasPoints: scienceInfo!.analysis.mediasPoints,
+						opening: scienceInfo!.analysis.opening,
+						rectFunction: scienceInfo!.analysis.rectFunction,
+						transversalAvgs: scienceInfo!.analysis.transversalAvgs,
+					},
+					lamp: data,
+					width: spectrum.imgWidth,
+					height: spectrum.imgHeight,
+					countCheckpoints,
+					segmentWidth: segmentWidth,
+					fitFunction: "linal-regression",
+				});
+			}
+			setSpectrumsData((prev) =>
+				[
+					...prev.filter((s) => s.data.id !== spectrum.id),
+					{ data: { ...spectrum }, image: data, analysis: result },
+				].sort(
+					(a, b) =>
+						a.data.imgTop - b.data.imgTop || a.data.imgLeft - b.data.imgLeft,
+				),
+			);
+		}
+		setState("ready");
+	}, [
+		observationId,
+		observationImage,
+		spectrums,
+		spectrumsData.find,
+		scienceAnalysis,
+	]);
 
-  return (
-    <Card>
-      <CardContent>
-        {state === "waiting" && <span>Waiting definition of spectrums</span>}
-        {state === "running" && <span className={cn("icon-[ph--spinner-bold] animate-spin")} />}
-        {state === "ready" && (
-          <>
-            {scienceAnalysis && (
-              <>
-                <ImageWithPixelExtraction
-                  title="Science Spectrum"
-                  src={`/spectrum/${spectrums[2].id}/image`}
-                  imageAlt="Pixel-by-pixel analysis of science spectrum to extract spectrum function."
-                  pointsWMed={scienceAnalysis!.mediasPoints}
-                  drawFunction={scienceAnalysis!.rectFunction}
-                  opening={scienceAnalysis!.opening}
-                />
-                <SimpleFunctionXY data={scienceAnalysis!.transversalAvgs} />
-              </>
-            )}
-            {lamp1Analysis && (
-              <>
-                <ImageWithPixelExtraction
-                  title="Lamp 1 Spectrum"
-                  src={`/spectrum/${spectrums[1].id}/image`}
-                  imageAlt="Pixel-by-pixel inference of the scientific spectrum of comparison lamp 1."
-                  pointsWMed={lamp1Analysis!.mediasPoints}
-                  drawFunction={lamp1Analysis!.rectFunction}
-                  opening={lamp1Analysis!.opening}
-                />
-                <SimpleFunctionXY data={lamp1Analysis!.transversalAvgs} />
-              </>
-            )}
-
-            {/* 
-						<ImageWithPixelExtraction
-							title="Lamp 2 Spectrum"
-							src={`/spectrum/${spectrums[2].id}/image`}
-							imageAlt="Pixel-by-pixel inference of the scientific spectrum of comparison lamp 2."
-							pointsWMed={specAnalysis!.lamp2MediasPoints}
-							drawFunction={specAnalysis!.lamp2Function!}
-							perpendicularFunctions={specAnalysis!.lamp2TransversalFunctions}
-							opening={specAnalysis!.lamp2AvgOpening}
-						/>
-						<SimpleFunctionXY data={specAnalysis!.lamp2TransversalAvgs} /> */}
-          </>
-        )}
-        {/* {isLoading || !specAnalysis ? (
-          <></>
-        ) : (          
-          <div className="flex flex-col content-center justify-center gap-4">
-            <div id="Spectrum-Extracted-Science">
-              <ImageWithPixelExtraction
-                title="Science Spectrum"
-                image={science!}
-                imageAlt="Pixel-by-pixel analysis of science spectrum to extract spectrum function."
-                pointsWMed={specAnalysis.scienceMediasPoints}
-                drawFunction={specAnalysis.scienceFunction!}
-                perpendicularFunctions={specAnalysis.scienceTransversalFunctions}
-                opening={specAnalysis.scienceAvgOpening}
-              />
-              <SimpleFunctionXY data={specAnalysis.scienceTransversalAvgs} />
-            </div>
-
-            <div id="Spectrum-Extracted-Lamp1">
-              <ImageWithPixelExtraction
-                title="Lamp 1 Spectrum"
-                image={lamp1!}
-                imageAlt="Pixel-by-pixel inference of the scientific spectrum of comparison lamp 1."
-                pointsWMed={specAnalysis.lamp1MediasPoints}
-                drawFunction={specAnalysis.lamp1Function!}
-                perpendicularFunctions={specAnalysis.lamp1TransversalFunctions}
-                opening={specAnalysis.lamp1AvgOpening}
-              />
-              <SimpleFunctionXY data={specAnalysis.lamp1TransversalAvgs} />
-            </div>
-
-            <div id="Spectrum-Extracted-Lamp2">
-              <ImageWithPixelExtraction
-                title="Lamp 2 Spectrum"
-                image={lamp2!}
-                imageAlt="Pixel-by-pixel inference of the scientific spectrum of comparison lamp 2."
-                pointsWMed={specAnalysis.lamp2MediasPoints}
-                drawFunction={specAnalysis.lamp2Function!}
-                perpendicularFunctions={specAnalysis.lamp2TransversalFunctions}
-                opening={specAnalysis.lamp2AvgOpening}
-              />
-              <SimpleFunctionXY data={specAnalysis.lamp2TransversalAvgs} />
-            </div>
-          </div>
-        )} */}
-      </CardContent>
-    </Card>
-  )
+	return (
+		<Card>
+			<CardContent>
+				{state === "waiting" && <span>Waiting definition of spectrums</span>}
+				{state === "running" && (
+					<span className={cn("icon-[ph--spinner-bold] animate-spin")} />
+				)}
+				{state === "ready" &&
+					spectrumsData &&
+					spectrumsData.map((sd, i) => (
+						<div key={`Spectrum Analysis ${sd.data.id}`}>
+							<ImageWithPixelExtraction
+								title={`Spectrum ${i}`}
+								image={`/spectrum/${sd.data.id}/image?ts=${Date.now()}`}
+								imageAlt="Pixel-by-pixel analysis of spectrum to extract spectrum function."
+								pointsWMed={sd.analysis.mediasPoints}
+								drawFunction={sd.analysis.rectFunction}
+								opening={sd.analysis.opening}
+							/>
+							<SimpleFunctionXY data={sd.analysis.transversalAvgs} />
+						</div>
+					))}
+			</CardContent>
+		</Card>
+	);
 }
