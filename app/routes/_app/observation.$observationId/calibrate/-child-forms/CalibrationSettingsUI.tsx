e85e722be8ba43417a244ad01e1a05cde4041ed4
valdornix <valdornix@gmail.com>
2026-08-24 14:42:42 -0300

    fix: [ID-274] 🔘 Cambiar estilo de botón (importar lamparas de material)

--- app/routes/_app/observation.$observationId/calibrate/-child-forms/CalibrationSettingsUI.tsx
+++ app/routes/_app/observation.$observationId/calibrate/-child-forms/CalibrationSettingsUI.tsx
@@ -79,7 +79,7 @@ export const CalibrationSettingsUI = withForm({
                 className="w-full border-r-0 bg-white shadow-none hover:cursor-pointer hover:bg-white"
                 onClick={() => setIsLoadLampModalOpen(true)}
               >
-                <span className="icon-[ph--file-plus] size-5" />
+                <span className="icon-[ph--file-plus] size-5 text-gray-600 group-hover:text-orange-500" />
               </Button>
               {isLoadLampModalOpen && (
                 <form.AppField name="material">
